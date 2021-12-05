a1295cd2584050f67b9bb4876309ccf202cb030f
hatedyouth <neverxforget@mail.ru>
2021-12-05 17:44:57 +0300

    dz1_finalcommit

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -2,25 +2,24 @@
 //  ProfileView.swift
 //  Netology_IB_Instruments
 //
-//  Created by Тимур Турлыкин on 04.12.2021.
+//  Created by Тимур Турлыкин on 05.12.2021.
 //
 
 import UIKit
 
 class ProfileView: UIView {
 
-    @IBOutlet weak var imageZhdun: UIImageView!
-    
     @IBOutlet weak var labelName: UILabel!
     
-    @IBOutlet weak var labelBirthdate: UILabel!
+    @IBOutlet weak var labelBirrthdate: UILabel!
     
-
     @IBOutlet weak var labelCity: UILabel!
     
 
-    @IBOutlet weak var labelDescription: UITextView!
+    @IBOutlet weak var textDescription: UITextView!
     
 
+    @IBOutlet weak var imageZhdun: UIImageView!
+    
 
 }
