6a778f468710f469f94c260eeb804061f927fb75
hatedyouth <neverxforget@mail.ru>
2021-12-07 23:41:01 +0300

    dz1_dorabotkacommit

--- Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/ProfileView.swift
@@ -8,18 +8,12 @@
 import UIKit
 
 class ProfileView: UIView {
-
+    
     @IBOutlet weak var labelName: UILabel!
-    
     @IBOutlet weak var labelBirrthdate: UILabel!
-    
     @IBOutlet weak var labelCity: UILabel!
-    
-
     @IBOutlet weak var textDescription: UITextView!
-    
-
     @IBOutlet weak var imageZhdun: UIImageView!
     
-
+    
 }
--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    func showProfile() {
         view.backgroundColor = .systemPink
         let profileView = (Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)![0])
         self.view.addSubview(profileView as! ProfileView)
+        (profileView as! UIView).frame = self.view.bounds
     }
-    
-    
-    
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        showProfile()
+        //view.backgroundColor = .systemPink
+        //let profileView = (Bundle.main.loadNibNamed("ProfileView", owner: self, options: nil)![0])
+        //self.view.addSubview(profileView as! ProfileView)
+    }
 }
